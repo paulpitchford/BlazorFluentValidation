47afb32b962e34995b1a21292d69bf6e64271dc2
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-08 17:10:56 +0000

    Add project files.

---
+++ BlazorFluentValidation/Data/Person.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+
+namespace BlazorFluentValidation.Data
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ BlazorFluentValidation/Validators/PersonValidator.cs
@@ -0,0 +1,24 @@
+﻿using BlazorFluentValidation.Data;
+using FluentValidation;
+
+namespace BlazorFluentValidation.Validators
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(p => p.FirstName)
+                .NotEmpty().WithMessage("You must enter your first name")
+                .MaximumLength(50).WithMessage("First name cannot be longer than 50 characters");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("You must enter your last name")
+                .MaximumLength(50).WithMessage("First name cannot be longer than 50 characters");
+
+            RuleFor(p => p.Age)
+                .NotNull().WithMessage("You must enter your age.")
+                .GreaterThanOrEqualTo(1).WithMessage("Your age must be greater than 0.")
+                .LessThan(150).WithMessage("Your age must be less than 150.");
+        }
+    }
+}
